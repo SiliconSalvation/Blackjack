e9a026bbeeb364dfce9c91faf74cbbc619c7f889
kazuma2448 <43222577+kazuma2448@users.noreply.github.com>
2019-04-22 16:11:44 -0400

    Update Card.java

--- blackjack/BlackJack/src/Card.java
+++ blackjack/BlackJack/src/Card.java
@@ -1,16 +1,28 @@
 import java.util.Objects;
-
+/**
+ * The Card class represents a card, as played in blackjack
+ * @author Nick Stauffer
+ * @author staunw01@pfw.edu
+ */
 public class Card {
-	private int value;
-	
+	private int value;			//An integer representing the value of the card
+	/**
+	 * Constructor that sets the value of the card
+	 * @param value
+	 */
 	public Card(int value) {
 		this.value = value;
 	}
-	
+	/**
+	 * Getter for the value field
+	 * @return an integer representing the card's value
+	 */
 	public int getValue() {
 		return this.value;
 	}
-	
+	/**
+	 * Determines equality of a card object to this one
+	 */
 	public final boolean equals(Object o) {
 		if(this == o) {
 			return true;
@@ -24,5 +36,5 @@ public class Card {
 		Card card = (Card) o;
 		
 		return Objects.equals(value, card.getValue());
-}
+	}
 }
