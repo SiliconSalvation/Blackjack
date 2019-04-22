c3bc049453150a688dbdafa26223a42743ce92b9
kazuma2448 <43222577+kazuma2448@users.noreply.github.com>
2019-04-22 16:18:48 -0400

    Update Hand.java

--- blackjack/BlackJack/src/Hand.java
+++ blackjack/BlackJack/src/Hand.java
@@ -1,23 +1,25 @@
 import java.util.ArrayList;
-
+/**
+ * The Hand class represents a hand in blackjack
+ * @author kazuma
+ * @author staunw01@pfw.edu
+ */
 public class Hand {
 	
-	private ArrayList<Card> cards;
-	private boolean standOnHand;
+	private ArrayList<Card> cards;			//An ArrayList of Card objects
+	private boolean isStanding;				//A boolean representing if the player is standing on the hand
 	
+	/**
+	 * Constructor that instantiates the fields
+	 */
 	public Hand() {
 		this.cards = new ArrayList<Card>();
-		this.standOnHand = false;
+		this.isStanding = false;
 	}
-
-	public ArrayList<Card> getCards() {
-		return cards;
-	}
-
-	public void setCards(ArrayList<Card> cards) {
-		this.cards = cards;
-	}
-	
+	/**
+	 * Determines if the the hand has two and only two cards that are the same
+	 * @return a boolean value representing of the hand has a pair
+	 */
 	public boolean hasPair() {
 		if(cards.get(0).equals(cards.get(1)) && cards.size() == 2) {
 			return true;
@@ -25,11 +27,18 @@ public class Hand {
 			return false;
 		}
 	}
-	
+	/**
+	 * Adds a card to this hand
+	 * @param card a Card object to be added to this hand
+	 */
 	public void addCard(Card card) {
 		cards.add(card);
 	}
-	
+	/**
+	 * determines the total value of the hand. If there is an ace and the value 
+	 * is greater than 21, then subtracts 10 from the hand
+	 * @return an integer value determining the value of the hand
+	 */
 	public int getHandTotal() {
 		int total = 0;
 		
@@ -43,7 +52,10 @@ public class Hand {
 		
 		return total;
 	}
-	
+	/**
+	 * Determines if the hand has an ace
+	 * @return a boolean representing if the hand has an ace
+	 */
 	public boolean hasAce() {
 		boolean hasAce = false;
 		
@@ -56,13 +68,33 @@ public class Hand {
 		
 		return hasAce;
 	}
-	
+	/**
+	 * Getter if the player held on the hand
+	 * @return
+	 */
 	public boolean isHolding() {
-		return standOnHand;
+		return isStanding;
 	}
-
-	public void setStandOnHand(boolean decision) {
-		this.standOnHand = decision;
+	/**
+	 * Sets the field isStanding
+	 * @param standing
+	 */
+	public void setStandOnHand(boolean standing) {
+		this.isStanding = standing;
 		
+	}
+	/**
+	 * Getter for cards field
+	 * @return an ArrayList of card objects
+	 */
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+	/**
+	 * sets the current hand with an ArrayList of card objects
+	 * @param cards an ArrayList of card objects
+	 */
+	public void setCards(ArrayList<Card> cards) {
+		this.cards = cards;
 	}
 }
