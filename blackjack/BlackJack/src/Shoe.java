b123b606e0c221f62b22efd08f6f4fd3a892575f
kazuma2448 <43222577+kazuma2448@users.noreply.github.com>
2019-04-22 16:12:42 -0400

    Update Shoe.java

--- blackjack/BlackJack/src/Shoe.java
+++ blackjack/BlackJack/src/Shoe.java
@@ -1,20 +1,28 @@
 import java.util.Random;
-
+/**
+ * The Shoe class represents a blackjack shoe
+ * @author Nick Stauffer
+ * @author staunw01@pfw.edu
+ */
 public class Shoe {
-	private Card[] cards = new Card[312];
+	private Card[] cards;		//Array of card objects
 	
-	public Shoe() {
-		int[] cardValues = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
-		
-//		for(int i = 0; i < cards.length; i++) {
-//			cards[i] = new Card(cardValues[i % 13]);
-//		}
+	/**
+	 * Constructor that instantiates the cards field using the amount of decks being played
+	 * @param numberOfDecks the amount of decks in the shoe
+	 */
+	public Shoe(int numberOfDecks) {
+		this.cards = new Card[numberOfDecks * 52];
+		final int[] cardValues = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
 		
 		for(int i = 0; i < cards.length; i++) {
-			cards[i] = new Card(11);
+			cards[i] = new Card(cardValues[i % 13]);
 		}
+		
 	}
-	
+	/**
+	 * Randomizes the cards in the deck
+	 */
 	public void shuffle() {
 		Random rand = new Random(); 
         
@@ -29,11 +37,18 @@ public class Shoe {
                
         } 
 	}
-	
+	/**
+	 * Returns the card in the Array at param count
+	 * @param count the card to be drawn from the array
+	 * @return
+	 */
 	public Card drawCard(int count) {
 		return cards[count];
 	}
-	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getNumberOfCardsInShoe() {
 		return cards.length;
 	}
